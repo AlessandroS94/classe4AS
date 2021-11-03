d3deeb6d9be9e51fbb5b252e39135a66c46b0873
Alessandro <alex_994@ymail.com>
2021-11-03 19:30:45 +0100

    array monodimensionale

--- puntatori.cpp
+++ puntatori.cpp
@@ -5,13 +5,16 @@ using namespace std;
 int main(){
     int num = 20;
     int *pnum;
-    cout<<" numero int:";
+    cout<<"numero int: \t";
     cout<<num<<endl;
     cout<<&num<< endl<<endl;
     pnum = &num;
-    cout<< "contenuto pnum :";
+    cout<< "contenuto pnum:\t";
     cout<< pnum << endl;
-    cout<< "indirizzo di pnum:";
+    cout<< "indirizzo di pnum:\t";
     cout<<&pnum << endl;
+    int *pnum1 = &num;
+    cout<< "indirizzo di pnum:\t";
+    cout<<pnum1 << endl;
     return 0;
 }
