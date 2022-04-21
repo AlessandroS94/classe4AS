c4e27ef8355a137127da54eadcb81a7873b7b515
Alessandro <alex_994@ymail.com>
2022-04-21 08:04:04 +0200

    update example php

--- esercizio2/index.php
+++ esercizio2/index.php
@@ -45,7 +45,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </footer>
 </div>
-
+<?php
+function somma($a, $b) {
+    $somma = $a + $b;
+    return $somma;
+}
+$somma = somma(3,5); // $somma sarà uguale ad 8
+echo($somma);
+?>
 
     
   </body>
